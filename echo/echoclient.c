bd0b2ea766add46dd499cdcd38679796fd451e85
pig19980 <dotheg30@gmail.com>
2024-08-17 13:54:43 +0900

    Add echo server and client

---
+++ echo/echoclient.c
@@ -0,0 +1,24 @@
+#include "csapp.h"
+
+int main(int argc, char **argv) {
+	int clientfd;
+	char buf[MAXLINE];
+	rio_t rio;
+
+	if (argc != 3) {
+		fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
+		exit(0);
+	}
+
+	clientfd = Open_clientfd(argv[1], argv[2]);
+	printf("Connected to server (%s, %s)\n", argv[1], argv[2]);
+	Rio_readinitb(&rio, clientfd);
+
+	while (Fgets(buf, MAXLINE, stdin) != NULL) {
+		Rio_writen(clientfd, buf, strlen(buf));
+		Rio_readlineb(&rio, buf, MAXLINE);
+		Fputs(buf, stdout);
+	}
+	Close(clientfd);
+	exit(0);
+}

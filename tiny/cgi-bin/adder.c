645d52bb728950915ab8942a2a6907dee1236c82
pig19980 <dotheg30@gmail.com>
2024-08-17 19:33:02 +0900

    Fix adder.c in tiny/cgi-bin more safety

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -7,7 +7,7 @@
 int main(void) {
 	char *buf, *p;
 	char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
-	int n1 = 0, n2 = 0;
+	int n1 = 0, n2 = 0, cont_len = 0;
 
 	if ((buf = getenv("QUERY_STRING")) != NULL) {
 		p = strchr(buf, '&');
@@ -19,14 +19,13 @@ int main(void) {
 		// sscanf(buf, "%d&%d", &n1, &n2);
 	}
 
-	sprintf(content, "Welcome to add.com: ");
-	sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
-	sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, n1, n2,
-			n1 + n2);
-	sprintf(content, "%sThanks for visiting!\r\n<p>", content);
+	cont_len += sprintf(content + cont_len, "Welcome to add.com: ");
+	cont_len += sprintf(content + cont_len, "THE Internet addition portal.\r\n<p>");
+	cont_len += sprintf(content + cont_len, "The answer is: %d + %d = %d\r\n<p>", n1, n2, n1 + n2);
+	cont_len += sprintf(content + cont_len, "Thanks for visiting!\r\n<p>");
 
 	printf("Connection: close\r\n");
-	printf("Content-length: %d\r\n", (int)strlen(content));
+	printf("Content-length: %d\r\n", cont_len);
 	printf("Content-type: text/html\r\n\r\n");
 	printf("%s", content);
 	fflush(stdout);
